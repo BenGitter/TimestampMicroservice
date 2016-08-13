90749d88b6760e34f383155f323432f5719fa1a7
Benjamin <benjaminvanzwienen@live.nl>
2016-08-13 23:15:21 +0200

    Add post possibility

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.post('/data', function(req, res){
+	var value = req.query.value;
+	console.log(value);
+	res.end(value);
 });
 
 app.listen(port, function () {
